142f9928ef526c8c5b8df56ef1b706e804fd88c8
unknown <isc.eulogio@gmail.com>
2022-03-23 13:59:49 -0600

    cloud t variables

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -7,10 +7,15 @@ terraform {
     }
   }
 }
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
+
 //credenciales
 provider "aws" {
-access_key = "xxxxxxxx"
-secret_key = "xxxxxxx"
+access_key = var.AWS_ACCESS_KEY_ID
+secret_key = var.AWS_SECRET_KEY_ID
 region = "us-east-2"
 }
 
